3697e50a017fa9f9c3c75df9d0e58dd9fc743eeb
Ashot Svazyan <ashot.svazyan222@gmail.com>
2026-02-07 17:23:22 +0300

    feat(core): upd applications service

--- services/applications/models.py
+++ services/applications/models.py
@@ -1,10 +1,27 @@
-from packages.kernel.adapters import ModelAdapter
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from applications.types import ApplicationCertificationService, ApplicationCertificationStatus
+from packages.kernel.adapters import DocumentModelAdapter
 from packages.kernel.utils import t
 
 
-class Application(ModelAdapter):
+class ApplicationCertification(DocumentModelAdapter):
+    service = models.CharField(t("Услуга"), choices=ApplicationCertificationService.choices, max_length=32)
+    status = models.CharField(
+        t("Статус"),
+        choices=ApplicationCertificationStatus.choices,
+        default=ApplicationCertificationStatus.SENT_TO_AUTHORITY,
+        max_length=32,
+    )
+    registration_date = models.DateField(t("Дата регистрации"), null=True, blank=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(t("ID объекта"))
+    target = GenericForeignKey("content_type", "object_id")
 
     class Meta:
         ordering = ("-created_at",)
-        verbose_name = t("Заявки")
-        verbose_name_plural = t("Заявка")
+        verbose_name = t("Заявка на сертификацию")
+        verbose_name_plural = t("Заявки на сертификацию")
--- services/applications/types.py
+++ services/applications/types.py
@@ -1,6 +1,34 @@
 from typing import NewType, TypedDict
 
+from django.db import models
+
+from packages.kernel.utils import t
+
 ApplicationId = NewType("ApplicationId", int)
+
+
+class ApplicationCertificationService(models.TextChoices):
+    RESEARCH_GMO = "research_gmo", t("Заявка на предоставление услуг (на исследование на ГМО)")
+    SEEDS_QUALITY = "seed_quality", t("Заявка на определение сортовых качеств семян")
+    SEEDS_SOWING = "seeds_sowing", t("Заявка на определение посевных (посадочных) качеств семян")
+    SEEDS_VOLUMES = "seeds_volumes", t("Заявка на досмотр объема ввозимых семян")
+
+
+class ApplicationCertificationStatus(models.TextChoices):
+    DRAFT = "draft", t("Черновик")
+    FORMED = "formed", t("Сформировано")
+    SENT_TO_AUTHORITY = "sent_to_authority", t("Направлена в ведомство")
+    REGISTERED_BY_AUTHORITY = "registered_by_authority", t("Зарегистрировано ведомством")
+    UNDER_REVIEW = "under_review", t("На рассмотрении")
+    IN_PROGRESS = "in_progress", t("В работе")
+    NEEDS_REVISION = "needs_revision", t("На доработку")
+    APPROBATION_DATE_APPROVED = "approbation_date_approved", t("Согласована дата апробации")
+    DECISION_PENDING = "decision_pending", t("Ожидается принятие решения")
+    PROCESSED = "processed", t("Обработана")
+    APPROVED = "approved", t("Закрыт")
+    REJECTED = "rejected", t("Отказано")
+    REJECTED_BY_AUTHORITY = "rejected_by_authority", t("Отказано ведомством")
+    CANCELLED = "cancelled", t("Аннулирован")
 
 
 class CreateApplicationDTO(TypedDict):
